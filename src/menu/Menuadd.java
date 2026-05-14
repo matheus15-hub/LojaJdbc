69c4a43161ac2bc50cd33c37a71dd86356ab3a47
Gabriel <gabrielhardtklipegokusuper@gmail.com>
2026-05-14 10:22:45 -0300

    Ajutando Menuadd: metodos sem parametros e uso de Contrustores conforme o grupo pediu

--- src/menu/Menuadd.java
+++ src/menu/Menuadd.java
@@ -1,16 +1,44 @@
 package menu;
 
-import DAO.ProdutoDAO;
+import java.util.Scanner;
 import entidades.Produto;
+import entidades.Clientes;
+import servicos.Produtoser;
+import servicos.Clienteser;
+import DAO.ProdutoDAO;
 import DAO.ClientesDAO;
 import entidades.Clientes;
 
-import java.util.Scanner;
-import servicos.Produtoser;
-import servicos.Clienteser;
-
 public class Menuadd {
     Scanner cin = new Scanner(System.in);
+    
+    public void Produtoadd(){
+        System.out.print("Nome do Produto: ");
+        String nome = cin.nextLine();
+        System.out.print("Preço: ");
+        float preco = cin.nextFloat();
+        System.out.print("Estoque: ");
+        int estoque = cin.nextInt();
+        cin.nextLine(); // Limpar o buffer do scanner anterior
+
+        Produto p = new Produto(0, nome, preco, estoque);
+        new Produtoser().adicionar(p);
+    }
+
+    public void Clienteadd(){
+        System.out.print("Nome do Cliente: ");
+        String nome = cin.nextLine();
+        System.out.print("CPF: ");
+        String cpf = cin.nextLine();
+
+        Clientes c = new Clientes(0, nome, cpf);
+        new Clienteser().adicionarCli(c);
+        }
+
+
+
+    
+    /*Codigo antigo que foi revisado
     public  void Produtoadd(Produto produto){
         System.out.print("Nome: ");
         produto.setNome_Produtos(cin.nextLine());
@@ -20,6 +48,6 @@ public class Menuadd {
         produto.setEstoque(cin.nextInt());
         new Produtoser().adicionar(produto);
         new ProdutoDAO().mostrarProduts(produto);
-    }
+    }*/
 
 }
