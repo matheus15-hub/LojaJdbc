18808edf64f38a84b25ece11dabe3f0864ae52ba
KevinR_XD69 <engs-kevinrichardt@camporeal.edu.br>
2026-05-28 19:51:14 -0300

    criando o AlterarVendedor

---
+++ src/menu/MenuAlterar/AlterarVendedor.java
@@ -0,0 +1,21 @@
+package menu.MenuAlterar;
+
+import menu.Menuprint;
+import servicos.Clienteser;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class AlterarVendedor {
+    Scanner sca = new Scanner(System.in);
+    int id_vendedor;
+    String certezadoVendedor;
+
+    public 
+    void nome_vendedor(){
+
+        while(true) {
+            System.out.println("Alteração no nome do Vendedor");
+        }
+    }
+}
