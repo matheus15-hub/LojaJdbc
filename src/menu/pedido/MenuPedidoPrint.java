75bf3a6bb21be362c2bcc4cf10cca8b910f0f8d4
Gabriel <gabrielhardtklipegokusuper@gmail.com>
2026-06-02 01:19:58 -0300

    Corrige chamada do menu de remocao de pedidos e fluxo do DAO

--- src/menu/pedido/MenuPedidoPrint.java
+++ src/menu/pedido/MenuPedidoPrint.java
@@ -7,36 +7,11 @@ public class MenuPedidoPrint {
     private Scanner sca = new Scanner(System.in);
 
     public void exibirMenuPrint() {
-        while (true) {
-            System.out.println("\n--- CONSULTAS DE PEDIDOS ---");
-            System.out.println("1 - Listar Todos os Pedidos (Detalhado)");
-            System.out.println("2 - Relatório: Faturamento por Vendedor");
-            System.out.println("3 - Relatório: Produtos Mais Vendidos");
-            System.out.println("0 - Voltar");
-            System.out.print("Opção: ");
-
-            while (!sca.hasNextInt()) {
-                System.out.println("Digite apenas números!");
-                sca.next();
-            }
-            int opcao = sca.nextInt();
-            sca.nextLine();
-
-            if (opcao == 0) break;
-
-            switch (opcao) {
-                case 1:
-                    PedidoDAO.imprimirPedidoS();
-                    break;
-                case 2:
-                    PedidoDAO.relatorioVendasPorVendedor();
-                    break;
-                case 3:
-                    PedidoDAO.relatorioProdutosMaisVendidos();
-                    break;
-                default:
-                    System.out.println("Opção inválida!");
-            }
-        }
+        System.out.println("\n--- CONSULTA DETALHADA DE PEDIDOS ---");
+        
+        PedidoDAO.imprimirPedidoS();
+        
+        System.out.print("\nPressione ENTER para voltar ao menu anterior...");
+        sca.nextLine();
     }
 }
